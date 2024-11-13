2a45dd3ab65f45bbedd7e34505fa5db774108770
Jean Carlos <je.franco@duocuc.cl>
2024-11-13 16:11:03 -0300

    Se agrega responsividad y mejoras a la vista login

--- compraYahorraAPP/urls.py
+++ compraYahorraAPP/urls.py
@@ -3,4 +3,5 @@ from .views import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('inicio/', views.inicio, name='inicio'),
 ]
--- compraYahorraAPP/views/views.py
+++ compraYahorraAPP/views/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def inicio(request):
+    return render(request, 'inicio.html')
